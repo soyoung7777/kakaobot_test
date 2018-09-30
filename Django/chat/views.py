22f4675ee12eae5862c0cc2f18c17a9170557353
sihyun0107 <sihyun0107@naver.com>
2018-10-01 00:33:10 +0900

    no message

--- Django/chat/views.py
+++ Django/chat/views.py
@@ -502,6 +502,7 @@ def message(request):
 def index(request):
     print("===call index function===")
     print("request : "+str(request))
-    msg = str(request)
+    #msg = str(request)
+    msg = "abc"
     return render_to_response('index.html', {'message': msg})
     #return render(request, 'chat/index.html')
