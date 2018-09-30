261ddb57bdbd1e632926bb192ac0b035c16f4f26
sihyun0107 <sihyun0107@naver.com>
2018-09-30 21:37:59 +0900

    no message

--- Django/chat/SubwayInfo.py
+++ Django/chat/SubwayInfo.py
@@ -134,12 +134,18 @@ def get_subway_station_and_number_information(subwayData):
     for d in direction:
         if "상행" in d:
             StationExistName = getStationExistSimple(stationName, current_laneID, 1)
+            if eq(StationExistName, "error"):
+                text +="공공데이터에 문제가 생겼어요😂😂\n10초 뒤에 다시 이용해주시겠어요?\n꼭 다시 오셔야해요❤"
+                return text
             if not eq(StationExistName,"none"):
                 text +="💌["+stationName+" "+current_laneName+" "+d+"]💌\n"
                 text +=StationExistName+"\n\n"
         else:
         #text +="💌["+stationName+" "+current_laneName+" "+direction[-1]+"]💌\n"
             StationExistName = getStationExistSimple(stationName, current_laneID, 2)
+            if eq(StationExistName, "error"):
+                text +="공공데이터에 문제가 생겼어요😂😂\n10초 뒤에 다시 이용해주시겠어요?\n꼭 다시 오셔야해요❤"
+                return text
             if not eq(StationExistName,"none"):
                 text +="💌["+stationName+" "+current_laneName+" "+d+"]💌\n"
                 text +=StationExistName+"\n\n"
