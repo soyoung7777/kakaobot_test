73515beff336cd61c00b1cb1af20e1c67c3cdb09
sihyun0107 <sihyun0107@naver.com>
2018-10-13 13:35:52 +0900

    no message

--- Django/chat/SubwayInfo.py
+++ Django/chat/SubwayInfo.py
@@ -85,8 +85,8 @@ def simple_get_subway_station_and_number_information(subwayData):
 
     day = getDayType()
     stationName = subwayData[0]
-    #subwayData[1] = re.sub('\'',"",subwayData[1])
-    #subwayData[1] = re.sub('수도권 ',"",subwayData[1])
+    subwayData[1] = re.sub('\'',"",subwayData[1])
+    subwayData[1] = re.sub('수도권 ',"",subwayData[1])
     print("stationName : "+subwayData[0])
     print("lineNumber : "+subwayData[1])
     print("lineNumber type : "+str(type(subwayData[1])))
@@ -168,8 +168,8 @@ def detail_get_subway_station_and_number_information(subwayData):
 
     day = getDayType()
     stationName = subwayData[0]
-    #subwayData[1] = re.sub('\'',"",subwayData[1])
-    #subwayData[1] = re.sub('수도권 ',"",subwayData[1])
+    subwayData[1] = re.sub('\'',"",subwayData[1])
+    subwayData[1] = re.sub('수도권 ',"",subwayData[1])
     print("stationName : "+subwayData[0])
     print("lineNumber : "+subwayData[1])
     print("lineNumber type : "+str(type(subwayData[1])))
