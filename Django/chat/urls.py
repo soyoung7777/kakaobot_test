f4c396043d0aeeb2b12cf330765ebbf84e3e1520
soyoung <thdud6418@gmail.com>
2018-10-23 15:27:01 +0900

    no message

--- Django/chat/urls.py
+++ Django/chat/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 	url(r'^message',views.message),
 	#url(r'^$',views.index),
 	url(r'^index/(?P<pk>.+)/$',views.index, name='index'),
-	path('',views.index, name='index')
+	path('',views.index, name='index'),
 	url(r'^pathFind/(?P<pk>.+)/$',views.pathFind, name='pathFind'),
 	path('',views.pathFind, name='pathFind')
 ]
