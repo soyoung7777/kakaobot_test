68ba7adfe65c58351af1a4bede8a5906f8248e2d
sihyun0107 <sihyun0107@naver.com>
2018-11-03 21:36:11 +0900

    no message

--- Django/chat/SubwayInfo.py
+++ Django/chat/SubwayInfo.py
@@ -365,7 +365,7 @@ def getLaneID(laneName):
     return open_data_subwayID
 
 def getStationInfo(myStationName):
-    myStationName = myStationName.replace(r'\((.*?)\)','')
+    myStationName = re.sub('\((.*?)\)','',myStationName)
     print("myStationName : "+myStationName)
     myKey = "2Y3C1Vf5IqtpTOyTtlHh1zhP2SJSByC9xqsjCDo/4FQ"
     encKey = urllib.parse.quote_plus(myKey)
