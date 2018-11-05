d48d38be3c2f96fea996eda7f55a757504e41dbe
sihyun0107 <sihyun0107@naver.com>
2018-11-05 21:32:03 +0900

    no message

--- Django/chat/SubwayInfo.py
+++ Django/chat/SubwayInfo.py
@@ -407,8 +407,8 @@ def simple_get_schedule(stationName, day, direction, laneName):
         print("현재 시간 : "+str(now.minute))
         if now.minute < int(re.sub('\((.*?)\)',"",t)):
             subway_direction =re.search('\((.*?)\)',t).group()
-            subway_direction = re.sub("(","",subway_direction)
-            subway_direction = re.sub(")","",subway_direction)
+            subway_direction = re.sub("^\(","",subway_direction)
+            subway_direction = re.sub("^\)","",subway_direction)
             text += subway_direction+"행 "+str(now.hour)+"시 "+re.sub('\((.*?)\)',"",t)+"분 도착 예정"
             return text
 
